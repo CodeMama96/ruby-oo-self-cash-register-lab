6bddafeb4de488663ae471cf9b22d340b648f50f
Alicia Weenum <weenum.alicia@gmail.com>
2020-11-05 13:12:35 -0500

    commit tree d0ab8b1fb7a5544f7cd7369f3155e1ffd17882f7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,57 @@
+class CashRegister
+    attr_accessor :total, :discount, :title, :price, :quantity, :last_transaction
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount 
+        @cart = []
+    end
+
+    def add_item(title, price, quantity =1)
+        @total += price*quantity
+        count = 0
+        while count < quantity
+            @cart << [title,price]
+            count += 1
+        end
+        @last_transaction = price*quantity
+
+        
+    end
+
+    def apply_discount
+        # if
+        #     if discount == 10
+        #         self.total = total - total * 10 / 100
+        #     elsif @discount == 20
+        #         self.total = total - total * 20 / 100
+        #     elsif @discount == 50
+        #         self.total = total - total * 50 / 100
+        #     end
+        #   "After the discount, the total comes to $#{total}."
+        # else 
+        #     "There is no discount to apply."
+        # end
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            @discount != 0
+            self.total = total - total * discount / 100
+            "After the discount, the total comes to $#{total}."
+        end
+
+    end
+
+    def items
+        titles_only = []
+        @cart.each do |food|
+            titles_only << food[0]
+        end 
+        titles_only
+    end
+    
+    def void_last_transaction
+        self.total -= self.last_transaction.to_f
+    end
+end
 
